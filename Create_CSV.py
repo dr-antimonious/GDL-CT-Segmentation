a80524c80f7ba5b8e96444f2fe34a6c53221ab6a
dr-antimonious <21060575+dr-antimonious@users.noreply.github.com>
2025-08-15 22:10:56 +0200

    Update

--- Create_CSV.py
+++ Create_CSV.py
@@ -8,20 +8,23 @@ DATASET_INFO_PATH = DIRECTORY + "imageCHD_dataset_info.xlsx"
 SCAN_INFO_PATH = DIRECTORY + "imagechd_dataset_image_info.xlsx"
 CHD_NAMES = ['ASD', 'VSD', 'AVSD', 'ToF', 'TGA', 'CA', 'PA', 'PDA']
 
-dataset_info = pd.read_csv(filepath_or_buffer = DIRECTORY + "patient_info.csv")
+def main():
+    dataset_info = pd.read_csv(filepath_or_buffer = DIRECTORY + "patient_info.csv")
 
-chd_names = CHD_NAMES
-chd_counts = [int(dataset_info[chd].value_counts()[1.0]) for chd in CHD_NAMES]
-chds = sorted(zip(chd_names, chd_counts), key = lambda x: x[1])
-print(chds)
-
-for chd, _ in chds[:-1]:
-    dataset_info.drop(dataset_info.loc[dataset_info[chd] == 1].index, inplace = True)
-    chd_names.remove(chd)
+    chd_names = CHD_NAMES
     chd_counts = [int(dataset_info[chd].value_counts()[1.0]) for chd in CHD_NAMES]
-    chds = sorted(zip(chd_names, chd_counts), key = lambda x: x[1])
+    chds = sorted(zip(chd_names, chd_counts), key = lambda x: (x[1], x[0]))
     print(chds)
 
+    for chd, _ in chds[:-1]:
+        dataset_info.drop(dataset_info.loc[dataset_info[chd] == 1].index, inplace = True)
+        chd_names.remove(chd)
+        chd_counts = [int(dataset_info[chd].value_counts()[1.0]) for chd in CHD_NAMES]
+        chds = sorted(zip(chd_names, chd_counts), key = lambda x: (x[1], x[0]))
+        print(chds)
+
+if __name__ == '__main__':
+    main()
 # axial_count = [nib.load("C:\\Users\\leotu\\Downloads\\ImageCHD_dataset\\ImageCHD_dataset\\ct_" \
 #                          + str(x) + "_image.nii.gz") \
 #                .header['dim'][3] for x in dataset_info['index'].sort_values()]
