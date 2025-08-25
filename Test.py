20645ace5faa53b39ad4ee88ff26269db900af88
dr-antimonious <21060575+dr-antimonious@users.noreply.github.com>
2025-08-25 23:28:23 +0200

    Update

--- Test.py
+++ Test.py
@@ -1,13 +1,13 @@
 from collections import OrderedDict
-from numpy import unique, array
-from pandas import read_csv
-from torch import load, device, max, FloatTensor
+from numpy import unique, array, zeros
+from pandas import DataFrame, read_csv
+from torch import load, device, max
 from torch.nn.functional import one_hot
 from torch_geometric.loader import DataLoader
 from tqdm import tqdm
 
 from torchmetrics.classification.accuracy import Accuracy
-from torchmetrics.classification.precision_recall import Recall, Precision
+from torchmetrics.classification.precision_recall import Precision
 from torchmetrics.collections import MetricCollection
 
 from Network import CHD_GNN
@@ -15,13 +15,13 @@ from Utilities import __Load_Adjacency__, load_nifti, CHD_Dataset
 
 MODEL_DIRECTORY = "/home/ubuntu/proj/GDL-CT-Segmentation/MODELS/"
 DIRECTORY = "/home/ubuntu/proj/ImageCHD_dataset/"
-NUM_CLASSES = 8
 DEVICE = "cuda:0"
 FREQS = array([7610724117.0, 35496879.0,
                37714647.0, 83133666.0,
                105607395.0, 144954300.0,
                57576912.0, 53353236.0])
-WEIGHTS = FloatTensor(FREQS.sum() / FREQS).to(DEVICE)
+W_STEP = FREQS.sum() / FREQS
+WEIGHTS = W_STEP / W_STEP.sum()
 
 def compute_iou_dice(pred, target, num_classes):
     pred_oh = one_hot(pred, num_classes = num_classes).bool()
@@ -31,27 +31,15 @@ def compute_iou_dice(pred, target, num_classes):
     fn = (~pred_oh & target_oh).sum(dim = 0).float()
     union = tp + fp + fn
     valid = union > 0
-    weights = WEIGHTS[valid] / WEIGHTS[valid].sum().item()
-    iou = tp[valid] / (union[valid] + 1e-7) * weights
-    dice = 2 * tp[valid] / (2 * tp[valid] + fp[valid] + fn[valid] + 1e-7) * weights
-    mean_iou = iou.sum().item()
-    mean_dice = dice.sum().item()
-    return mean_iou, mean_dice
+    iou = tp[valid] / (union[valid] + 1e-7)
+    dice = 2 * tp[valid] / (2 * tp[valid] + fp[valid] + fn[valid] + 1e-7)
+    return iou, dice, valid
 
 def main():
-    print(WEIGHTS)
     dev = device(DEVICE)
-    model = CHD_GNN().to(dev)
-    checkpoint = load(MODEL_DIRECTORY + "gnn_90.checkpoint",
-                      map_location = dev)["MODEL_STATE"]
-    
-    new_state_dict = OrderedDict()
-    for k, v in checkpoint.items():
-        nk = k.replace("module.", "")
-        new_state_dict[nk] = v
-    
-    model.load_state_dict(new_state_dict)
-    model.eval()
+    epochs = list(range(35, 46))
+    epochs.extend(range(75, 86))
+    results = {}
 
     adjacency = __Load_Adjacency__(DIRECTORY + 'ADJACENCY/')
     test_metadata = read_csv(filepath_or_buffer = DIRECTORY + 'test_dataset_info.csv')
@@ -68,19 +56,69 @@ def main():
     
     metrics = MetricCollection([
         Accuracy(task = 'multiclass', average = None, num_classes = 8),
-        Precision(task = 'multiclass', average = None, num_classes = 8),
-        Recall(task = 'multiclass', average = None, num_classes = 8)],
+        Precision(task = 'multiclass', average = None, num_classes = 8)],
     ).to(dev)
 
-    for batch in tqdm(test_dataloader):
-        batch = batch.to(dev, non_blocking = True)
-        preds = model(x = batch.x, edges = batch.edge_index,
-                      batch = batch.batch, b_size = batch.num_graphs)
-        _, pred_labels = max(preds, dim = 1)
+    for epoch in epochs:
+        model = CHD_GNN().to(dev)
+        checkpoint = load(MODEL_DIRECTORY + "gnn_" + str(epoch) + ".checkpoint",
+                        map_location = dev)["MODEL_STATE"]
         
-        m1 = metrics(preds, batch.y)
-        dice, iou = compute_iou_dice(pred_labels, batch.y, 8)
-        print(m1, dice, iou)
+        new_state_dict = OrderedDict()
+        for k, v in checkpoint.items():
+            nk = k.replace("module.", "")
+            new_state_dict[nk] = v
+        
+        model.load_state_dict(new_state_dict)
+        model.eval()
+
+        tot_iou = zeros(8, dtype = float)
+        tot_dice = zeros(8, dtype = float)
+        tot_acc = zeros(8, dtype = float)
+        tot_prec = zeros(8, dtype = float)
+        tot_valid = zeros(8)
+
+        for batch in tqdm(test_dataloader):
+            batch = batch.to(dev, non_blocking = True)
+            preds = model(x = batch.x, edges = batch.edge_index,
+                        batch = batch.batch, b_size = batch.num_graphs)
+            _, pred_labels = max(preds, dim = 1)
+            
+            m = metrics(preds, batch.y)
+            iou, dice, valid = compute_iou_dice(pred_labels, batch.y, 8)
+
+            tot_iou[valid] += iou.tolist()
+            tot_dice[valid] += dice.tolist()
+            tot_acc[valid] += m['MulticlassAccuracy'].tolist()[valid]
+            tot_prec[valid] += m['MulticlassPrecision'].tolist()[valid]
+            tot_valid += valid
+
+            print(tot_valid)
+            print(tot_iou)
+            print(tot_dice)
+            print(tot_acc)
+            print(tot_prec)
+        
+        tot_iou /= tot_valid
+        tot_dice /= tot_valid
+        tot_acc /= tot_valid
+        tot_prec /= tot_valid
+
+        results.update([("MODEL_" + str(epoch), {
+            "IoU": tot_iou,
+            "Dice": tot_dice,
+            "Accuracy": tot_acc,
+            "Precision": tot_prec,
+            "Mean_IoU": (tot_iou * WEIGHTS).sum(),
+            "Mean_Dice": (tot_dice * WEIGHTS).sum(),
+            "Mean_Acc": (tot_acc * WEIGHTS).sum(),
+            "Mean_Prec": (tot_prec * WEIGHTS).sum()
+        })])
+        
+        break
+    
+    save = DataFrame(results)
+    save.to_csv(MODEL_DIRECTORY + "results.csv")
         
 if __name__ == '__main__':
     main()
